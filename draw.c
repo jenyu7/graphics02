482a69a75afff561eeb001fb6ab86f70cc37e0f9
jenyu7 <jyu7@stuy.edu>
2018-02-10 18:02:34 -0500

    added draw code

---
+++ draw.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+//line algorithm
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int dx = x1-x0;
+  int dy = y1-y0;
+  int octant = -1;
+
+  //change starting points
+  if (dy < 0){draw_line(x1, y1, x0, y0, s, c);}
+  //|m| <= 1, [+]
+  if (dy <= dx && dx*dy >= 0){octant = 1;}
+  //|m| > 1, [+]
+  else if (dy > dx && dx*dy >= 0){octant = 2;}
+  //|m| >= 1, [-]
+  else if (dy >= dx*-1 && dx*dy < 0){octant = 3;}
+  //|m| < 1, [-]
+  else if (dy < dx*-1 && dx*dy < 0){octant = 4;}
+
+  draw(octant, x0, y0, x1, y1, s, c);
+}
+
+//plots the points of the line in a while loop
+void draw(int o, int x, int y, int x1, int y1, screen s, color c){
+  if (!o){printf("Invalid octant.");return;}
+  int A = y1-y;
+  int B = -1*(x1-x);
+  int d;
+  //octants 1 & 5
+  if (o == 1){
+    d = 2*A + B;
+    while (x < x1){
+      plot(s, c, x, y);
+      if (d>0){y ++; d+= 2*B;}
+      x++;
+      d += 2*A;
+    }
+  }
+  //octants 2 & 6
+  else if (o == 2){
+    d = A + 2*B;
+    while (y < y1){
+      plot(s, c, x, y);
+      if (d < 0){x ++; d += 2*A;}
+      y++;
+      d += 2*B;
+    }
+  }
+  //octants 3 & 7
+  else if (o == 3){
+    d = 2*B - A;
+    while (y < y1){
+      plot(s, c, x, y);
+      if(d > 0){x--; d -= 2*A;}
+      y++;
+      d += 2*B;
+    }
+  }
+  //octants 4 & 8
+  else{
+    d = -2*A + B;
+    while(x > x1){
+      plot(s, c, x, y);
+      if (d < 0){y++; d += 2*B;}
+      x --;
+      d -= 2*A;
+    }
+  }
+}
