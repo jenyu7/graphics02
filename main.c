c0934190d2f111e2d4fe5b08245bc3fccea19031
jenyu7 <jyu7@stuy.edu>
2018-02-11 14:55:26 -0500

    added new test

--- main.c
+++ main.c
@@ -55,6 +55,37 @@ int main() {
     y += r%200; 
     draw_square(x, y, 50 , 40, s, c);
     }
+  
+  //Mr. DW's test code
+  //octants 1 and 5
+  draw_line(0, 0, XRES-1, YRES-1, s, c);
+  draw_line(0, 0, XRES-1, YRES / 2, s, c);
+  draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c);
+
+  //octants 8 and 4
+  c.blue = 255;
+  draw_line(0, YRES-1, XRES-1, 0, s, c);
+  draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+  draw_line(XRES-1, 0, 0, YRES/2, s, c);
+
+  //octants 2 and 6
+  c.red = 255;
+  c.green = 0;
+  c.blue = 0;
+  draw_line(0, 0, XRES/2, YRES-1, s, c);
+  draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+
+  //octants 7 and 3
+  c.blue = 255;
+  draw_line(0, YRES-1, XRES/2, 0, s, c);
+  draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+
+  //horizontal and vertical
+  c.blue = 0;
+  c.green = 255;
+  draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+  draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+  
   display(s);
   save_extension(s, "lines.png");
 }
